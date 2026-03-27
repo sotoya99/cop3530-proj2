3d429bcd171de7cf990cc3356247173db35cbf62
Santiago Otoya <santiagootoya05@gmail.com>
2026-03-26 21:24:00 -0400

    Created TST class with constructors

---
+++ TernarySearchTree.py
@@ -0,0 +1,9 @@
+class TSTNode:
+    def __init__(self, char):
+        self.char = char
+        self.left = None
+        self.mid = None
+        self.right = None
+        self.is_end_of_string = False
+
+    
